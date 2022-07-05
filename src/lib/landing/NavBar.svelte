15ddac5870502a5a5b3cb71c90f1b052d0ec551d
Below Bed <jordanmeta4677@gmail.com>
2022-07-06 04:52:52 +0800

    smooth transition when show search bar

--- src/lib/landing/NavBar.svelte
+++ src/lib/landing/NavBar.svelte
@@ -1,15 +1,17 @@
 <script>
   import Icon, { addIcon } from '@iconify/svelte';
+  import { fade, scale } from 'svelte/transition';
   import biCart from '@iconify/icons-bi/cart';
   import biPerson from '@iconify/icons-bi/person';
   import biSearch from '@iconify/icons-bi/search';
   import Delivery from "$lib/icons/delivery.svelte";
   import SearchButton from '$lib/SearchButton.svelte';
-  import SearchImg from '$lib/icons/search.png';
 
   addIcon('cart', biCart);
   addIcon('person', biPerson);
   addIcon('search', biSearch);
+  
+  let searchInputVisible = false;
 
   const menus = [
     { icon: 'person', path: '/account' },
@@ -21,12 +23,19 @@
   .NavMenuIcon {
     @apply text-white hover:bg-ksblack-aug focus:bg-ksblack-aug p-2 rounded-full transition-all text-gray-100;
   }
+
+  .MenuRight.Hide {
+    flex-basis: 0;
+    flex-grow: 0;
+    overflow: hidden;
+    opacity: .25;
+  }
 </style>
 
 <nav class="bg-ksblack w-full flex items-center">
-  <div class="md:w-2/3 md:mx-auto h-20 flex items-center justify-between">
+  <div class="md:w-3/4 md:mx-auto h-20 flex items-center">
     
-    <div class="flex-grow flex items-center text-white gap-x-4">
+    <div class="flex items-center text-white gap-x-4">
       <Delivery />
       <div>
         <div class="text-sm">For delivery, call us</div>
@@ -34,8 +43,21 @@
       </div>
     </div>
 
-    <div class="flex items-center gap-x-2">
-      <SearchButton />
+    <SearchButton 
+      inputVisible={searchInputVisible}
+      placeholder="What are you looking for..."
+      on:input-hidden={() => {
+        searchInputVisible = false;
+      }}
+      on:input-visible={() => {
+        searchInputVisible = true;
+      }}
+    />
+
+    <div 
+      class="MenuRight flex items-center gap-x-2 transition-all"
+      class:Hide={searchInputVisible}
+    >
       {#each menus as menu}
         <a 
           href={menu.path}
