09b5fcbde56ffeb4ec7a0bce545f676aebddf4c5
Below Bed <jordanmeta4677@gmail.com>
2022-07-06 06:33:40 +0800

    carousel indicator

--- src/lib/landing/HeroCarousel.svelte
+++ src/lib/landing/HeroCarousel.svelte
@@ -26,14 +26,28 @@
   })
 </script>
 
+<style>
+
+</style>
+
 {#each images as img, idx}
   {#if activeIndex == idx}
     <div 
       class="absolute top-0 bottom-0 left-0 right-0"
-      style={`background: url(${img}); background-position: center; background-size: cover; z-index: 1;`}
+      style={`background: url(${img}); background-position: center; background-size: cover;`}
       in:fly={{ duration: 400, x: 200, y: 0 }}
       out:fade={{ duration: 800 }}
     >
     </div>
   {/if}
 {/each}
+
+<div class="absolute flex items-center justify-center gap-4 h-24" style="bottom: 0; left: 0; right: 0;">
+  {#each images as img, idx}
+    {#if activeIndex == idx}
+      <div class="w-6 h-2 rounded-lg bg-ksgreen"></div>
+    {:else}
+      <div class="w-2 h-2 rounded-lg bg-white"></div>
+    {/if}
+  {/each}
+</div>
