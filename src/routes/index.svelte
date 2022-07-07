3a84f728a182ac7820e86032000a3daf33215670
Below Bed <jordanmeta4677@gmail.com>
2022-07-08 05:47:57 +0800

    add landing components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
+import About from '$lib/landing/About.svelte';
+import Features from '$lib/landing/Features.svelte';
 import Hero from '$lib/landing/Hero.svelte';
 	import NavBar from '$lib/landing/NavBar.svelte';
+import SpecialDeals from '$lib/landing/SpecialDeals.svelte';
 </script>
 
 <svelte:head>
@@ -9,23 +12,10 @@ import Hero from '$lib/landing/Hero.svelte';
 	<meta name="description" content="Kesato Tasty Food" />
 </svelte:head>
 
-<NavBar/>
-<Hero/>
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<div>
+	<NavBar/>
+	<Hero/>
+	<About/>
+	<Features/>
+	<SpecialDeals />
+</div>
