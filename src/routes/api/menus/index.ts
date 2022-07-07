939e2469f4d7ffe4c68bed4c5846ba8cea15881e
Below Bed <jordanmeta4677@gmail.com>
2022-07-08 05:47:12 +0800

    menus API

---
+++ src/routes/api/menus/index.ts
@@ -0,0 +1,25 @@
+import type { RequestHandler } from '@sveltejs/kit'
+import { z } from 'zod';
+import { search } from 'src/serv/menus'
+import { MenuTypeSchema } from 'src/types';
+
+const SearchParamsSchema = z.object({
+  type: MenuTypeSchema
+})
+
+export const get: RequestHandler = async ({ url }) => {
+  const parseResult = await MenuTypeSchema.safeParseAsync(url.searchParams.get('type'));
+  if (!parseResult.success) {
+    throw new Error('invalid search parameters');
+  }
+  const type = parseResult.data;
+  if (!type) {
+    throw new Error('type is undefined')
+  }
+  const menus = await search({ type });
+  return {
+    body: {
+      items: menus
+    }
+  }
+}
