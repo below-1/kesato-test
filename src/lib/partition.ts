ce35e3b7595c51517f4cd0acfda1546b84888315
Below Bed <jordanmeta4677@gmail.com>
2022-07-08 13:40:53 +0800

    Add utility function to partition array

---
+++ src/lib/partition.ts
@@ -0,0 +1,16 @@
+export default function partition<T>(items: Array<T>, n: number) {
+  let result: Array<Array<T>> = [];
+  let subresult: Array<T> = [];
+  for (let i = 1; i <= items.length; i++) {
+    subresult.push(items[i - 1]);
+    console.log(subresult);
+    if ((subresult.length % n) == 0) {
+      result.push(subresult);
+      subresult = [];
+    }
+  }
+  if (subresult.length) {
+    result.push(subresult);
+  }
+  return result;
+}
