7de2bd71023abbe7f2e3c4ab2f1c84418e28e8e0
Below Bed <jordanmeta4677@gmail.com>
2022-07-08 21:53:24 +0800

    Refactor Carousel Indicator

---
+++ src/lib/CarouselIndicator.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let nItems: number;
+  export let activeIndex: number;
+
+  $: range = Array(nItems).fill(0);
+</script>
+
+<div class="flex items-center justify-center gap-4 h-24">
+  {#each range as _, idx}
+    {#if activeIndex == idx}
+      <div class="w-6 h-2 rounded-lg bg-ksgreen"></div>
+    {:else}
+      <div class="w-2 h-2 rounded-lg bg-white"></div>
+    {/if}
+  {/each}
+</div>
