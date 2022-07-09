baf60629db4df0f97a7de75e46ce6994436415a8
Below Bed <jordanmeta4677@gmail.com>
2022-07-09 17:28:57 +0800

    Add CTO

---
+++ src/lib/landing/CTO.svelte
@@ -0,0 +1,37 @@
+<style>
+  .CTO {
+    background-image: url(/img/pages/home/bg-subscribe.jpg);
+    background-size: cover;
+    background-position: center;
+  }
+
+  .CTO input::placeholder {
+    color: #222;
+  }
+</style>
+
+<div class="CTO">
+  <div class="Container TinySection flex flex-col items-center justify-center text-white">
+    <h1 class="font-display text-4xl mb-6">Susbcribe for Promos, Discount & Deals</h1>
+    <img
+      src="/img/decor/wawe.svg"
+      class="h-4 mb-12"
+      alt="wave"
+    />
+    <div class="bg-white flex items-center rounded space-x-6 px-4 w-3/5">
+      <img
+        src="/img/icons/email.png"
+        class="h-4"
+        alt="email icon"
+      />
+      <input
+        class="flex-grow bg-white text-sm py-5 text-black"
+        style="outline: none;"
+        placeholder="Enter your email address..."
+      />
+      <button class="uppercase font-bold text-sm bg-ksred px-4 py-2">
+        subscribe now!
+      </button>
+    </div>
+  </div>
+</div>
